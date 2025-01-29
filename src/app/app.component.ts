bc1b0b56cbd6648814480b06aedb00b8c874e2af
Ramesh Parthasarathy <gitapriyam@gmail.com>
2025-01-29 08:40:39 -0800

    added the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, 
-  IonList, IonListHeader, IonMenuToggle, 
-  IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
+import {
+  IonApp, IonSplitPane, IonMenu, IonContent,
+  IonList, IonListHeader, IonMenuToggle,
+  IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { folderOutline, arrowForwardCircleOutline, arrowBackCircleOutline } from 'ionicons/icons';
 import { environment } from '../environments/environment';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +17,12 @@ import { environment } from '../environments/environment';
   styleUrls: ['app.component.scss'],
   imports: [RouterLink, RouterLinkActive, IonApp, IonSplitPane,
     IonMenu, IonContent, IonList, IonListHeader, IonMenuToggle,
-    IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet],
+    IonItem, IonIcon, IonLabel, IonRouterLink, IonRouterOutlet,
+    FooterComponent],
 })
 export class AppComponent {
   public appPages = environment.appPages;
-  
+
   constructor() {
     addIcons({ folderOutline, arrowForwardCircleOutline, arrowBackCircleOutline });
   }
